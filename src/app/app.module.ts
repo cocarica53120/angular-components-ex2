7e893dd779829c91dc24afa96c0a2f00fea9f2e2
cocarica53120 <alain.collet2@gmail.com>
2019-10-18 07:12:32 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { Comp1Component } from './comp1/comp1.component';
+import { Comp11Component } from './comp1/comp11/comp11.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, Comp1Component ],
+  declarations: [ AppComponent, HelloComponent, Comp1Component, Comp11Component ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
